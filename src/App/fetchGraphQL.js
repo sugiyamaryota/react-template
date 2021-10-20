97da98d03b315f037b394b54514a26af81d28633
sugiyama ryota <guqeeky6va8@gmail.com>
2021-10-20 15:54:02 +0900

    fix github token

--- src/App/fetchGraphQL.js
+++ src/App/fetchGraphQL.js
@@ -1,10 +1,10 @@
 async function fetchGraphQL(text, variables) {
-    const REACT_APP_GITHUB_AUTH_TOKEN = process.env.REACT_APP_GITHUB_AUTH_TOKEN;
+    const GITHUB_AUTH_TOKEN = process.env.GITHUB_AUTH_TOKEN;
     // Fetch data from GitHub's GraphQL API:
     const response = await fetch('https://api.github.com/graphql', {
       method: 'POST',
       headers: {
-        Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+        Authorization: `bearer ${GITHUB_AUTH_TOKEN}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
